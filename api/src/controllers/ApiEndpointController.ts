ccb0805563513efc7b8f8f3b8c4baf64f7dd8683
Guillaume Benard <guillaume.benard@epitech.eu>
2019-06-17 23:46:48 +0200

    [API] ApiEndpointController

---
+++ api/src/controllers/ApiEndpointController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+
+class ApiEndpointController {
+    public readonly endpoint = (_res: Request, res: Response) => {
+        res.status(200).json({
+            message: "Welcome to my API !"
+        });
+    };
+}
+
+
+export default ApiEndpointController;
