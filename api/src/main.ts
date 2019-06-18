40c330e3a6adbdcb90bb536be68a1965f4ab38a5
Guillaume Benard <guillaume.benard@epitech.eu>
2019-06-18 21:24:25 +0200

    [API] Split main initialization

--- api/src/main.ts
+++ api/src/main.ts
@@ -17,44 +17,48 @@ import AuthLocalController from "./controllers/AuthLocalController";
 import PasswordValidator from "./validators/PasswordValidator";
 
 
-const main = () => {
+function main(): void {
     const dbConfig = DbConfig.load("../ormconfig.js");
 
     createConnection(dbConfig)
         .then((conn: Connection) => {
             console.info("Connected to database");
 
-            // Create Repositories
-            const userRepository = new UserRepository(conn);
-
-            // Create Validators
-            const emailValidator = new EmailValidator();
-            const nameValidator = new NameValidator();
-            const passwordValidator = new PasswordValidator();
-
-            // Create Services
-            const passwordService = new PasswordService();
-
-            // Create Controllers
-            const apiEndpointController = new ApiEndpointController();
-            const authLocalController = new AuthLocalController(
-                userRepository,
-                emailValidator,
-                nameValidator,
-                passwordValidator,
-                passwordService
-            );
-
-            // Create router and server
-            const router = createRouter(
-                apiEndpointController,
-                authLocalController
-            );
-            const server = new Server(router);
+            const server = createServer(conn);
 
             server.start(3000);
         });
 };
+
+function createServer(conn: Connection): Server {
+    // Create Repositories
+    const userRepository = new UserRepository(conn);
+
+    // Create Validators
+    const emailValidator = new EmailValidator();
+    const nameValidator = new NameValidator();
+    const passwordValidator = new PasswordValidator();
+
+    // Create Services
+    const passwordService = new PasswordService();
+
+    // Create Controllers
+    const apiEndpointController = new ApiEndpointController();
+    const authLocalController = new AuthLocalController(
+        userRepository,
+        emailValidator,
+        nameValidator,
+        passwordValidator,
+        passwordService
+    );
+
+    // Create router and server
+    const router = createRouter(
+        apiEndpointController,
+        authLocalController
+    );
+    return new Server(router);
+}
 
 
 // Run only if executed directly (e.g: `node src/main.js`)
