d97732648c40117d6517e816dd6e640386d39155
Guillaume Benard <guillaume.benard@epitech.eu>
2019-06-20 14:23:52 +0200

    [API] Add /users/me to get User's own info

--- api/src/router.ts
+++ api/src/router.ts
@@ -2,19 +2,27 @@ import { Router } from "express";
 
 import ApiEndpointController from "./controllers/ApiEndpointController";
 import AuthLocalController from "./controllers/AuthLocalController";
+import UserController from "./controllers/UserController";
 
-import asyncHandler from "./middlewares/asyncHandler";
+import {  AuthenticatedOnlyMiddleware } from "./middlewares/authenticatedOnly";
+import asyncHandler from "./middlewares/utils/asyncHandler";
 
 
 export const createRouter = (
     apiEndpointController: ApiEndpointController,
-    authLocalController: AuthLocalController
+    authLocalController: AuthLocalController,
+    userController: UserController,
+
+    authenticatedOnly: AuthenticatedOnlyMiddleware
 ): Router => {
     const router = Router();
 
     router.get("/", apiEndpointController.endpoint);
 
     router.post("/auth/local/register", asyncHandler(authLocalController.registerUser));
+    router.post("/auth/local/login", asyncHandler(authLocalController.loginUser));
+
+    router.get("/users/me", asyncHandler(authenticatedOnly), userController.getMyInfo);
 
     return router;
 };
